d6ce352696fee32f6d1a0c776a48b4fa5b305042
RamyaKrishnanRK <ramyakrishnan496@gmail.com>
2019-06-10 12:52:45 +0530

    first

---
+++ app/src/main/java/com/example/blink/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.blink;
+
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView im_blink = (ImageView) findViewById(R.id.im_blink);
+        final Animation animation = new AlphaAnimation((float) 0.5, 0);
+        animation.setDuration(500);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.REVERSE);
+        im_blink.startAnimation(animation);
+    }
+}
